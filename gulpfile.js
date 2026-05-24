37cfb2ab0c90fa5b73274c26e51d2b691c60bfa9
MaksimDoronin <114738598+MaksimDoronin@users.noreply.github.com>
2026-05-24 11:43:57 +0300

    Адаптивная графика (#3)
    * feat: добавил и подключил фавиконки
    
    * feat: заполнил мета теги с ключ словами и описанием
    
    * fix: правлию настройки сборщика
    
    * feat: добавил и оптимизировал растровую графику и контентные иконки
    
    * feat: Подключил адаптивную графику всех контентных изображений и логотипов

--- gulpfile.js
+++ gulpfile.js
@@ -30,61 +30,65 @@ const PATHS_TO_STATIC = [
 ];
 let isDevelopment = true;
 
-export function processMarkup () {
+export function processMarkup() {
   return src(`${PATH_TO_SOURCE}**/*.html`)
     .pipe(htmlmin({ collapseWhitespace: !isDevelopment }))
     .pipe(dest(PATH_TO_DIST))
     .pipe(server.stream());
 }
 
-
-export function processStyles () {
+export function processStyles() {
   return src(`${PATH_TO_SOURCE}styles/*.scss`, { sourcemaps: isDevelopment })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      postUrl([
-        {
-          filter: '**/*',
-          assetsPath: '../',
-        },
-        {
-          filter: '**/icons/**/*.svg',
-          url: (asset) => asset.url.replace(
-            /icons\/(.+?)\.svg$/,
-            (match, p1) => `icons/stack.svg#${p1.replace(/\//g, '_')}`
-          ),
-          multi: true,
-        },
+    .pipe(
+      postcss([
+        postUrl([
+          {
+            filter: '**/*',
+            assetsPath: '../',
+          },
+          {
+            filter: '**/icons/**/*.svg',
+            url: (asset) =>
+              asset.url.replace(
+                /icons\/(.+?)\.svg$/,
+                (match, p1) => `icons/stack.svg#${p1.replace(/\//g, '_')}`,
+              ),
+            multi: true,
+          },
+        ]),
+        lightningcss({
+          lightningcssOptions: {
+            minify: !isDevelopment,
+          },
+        }),
       ]),
-      lightningcss({
-        lightningcssOptions: {
-          minify: !isDevelopment,
-        },
-      })
-    ]))
+    )
     .pipe(dest(`${PATH_TO_DIST}styles`, { sourcemaps: isDevelopment }))
     .pipe(server.stream());
 }
 
-export function processScripts () {
+export function processScripts() {
   const gulpEsbuild = createGulpEsbuild({ incremental: isDevelopment });
 
   return src(`${PATH_TO_SOURCE}scripts/*.js`)
-    .pipe(gulpEsbuild({
-      bundle: true,
-      format: 'esm',
-      // splitting: true,
-      platform: 'browser',
-      minify: !isDevelopment,
-      sourcemap: isDevelopment,
-      target: browserslistToEsbuild(),
-    }))
+    .pipe(
+      gulpEsbuild({
+        bundle: true,
+        format: 'esm',
+        // splitting: true,
+        platform: 'browser',
+        minify: !isDevelopment,
+        sourcemap: isDevelopment,
+        target: browserslistToEsbuild(),
+      }),
+    )
     .pipe(dest(`${PATH_TO_DIST}scripts`))
     .pipe(server.stream());
 }
 
-export function optimizeRaster () {
+export function optimizeRaster() {
   const RAW_DENSITY = 2;
   const TARGET_FORMATS = [undefined, 'webp']; // undefined — initial format: jpg or png
 
@@ -93,66 +97,68 @@ export function optimizeRaster () {
 
     for (const format of TARGET_FORMATS) {
       for (let density = RAW_DENSITY; density > 0; density--) {
-        formats.push(
-          {
-            format,
-            rename: { suffix: `@${density}x` },
-            width: ({ width }) => Math.ceil(width * density / RAW_DENSITY),
-            jpegOptions: { progressive: true },
-          },
-        );
+        formats.push({
+          format,
+          rename: { suffix: `@${density}x` },
+          width: ({ width }) => Math.ceil((width * density) / RAW_DENSITY),
+          jpegOptions: { progressive: true },
+        });
       }
     }
 
     return { formats };
   }
 
-  return src(`${PATH_TO_RAW}images/**/*.{png,jpg,jpeg}`)
+  return src(`${PATH_TO_RAW}images/**/*.{png,jpg,jpeg}`, { encoding: false })
     .pipe(sharp(createOptionsFormat()))
     .pipe(dest(`${PATH_TO_SOURCE}images`));
 }
 
-export function optimizeVector () {
+export function optimizeVector() {
   return src([`${PATH_TO_RAW}**/*.svg`])
     .pipe(svgo())
     .pipe(dest(PATH_TO_SOURCE));
 }
 
-export function createStack () {
+export function createStack() {
   return src(`${PATH_TO_SOURCE}icons/**/*.svg`)
     .pipe(stacksvg())
     .pipe(dest(`${PATH_TO_DIST}icons`));
 }
 
-export function copyStatic () {
-  return src(PATHS_TO_STATIC, { base: PATH_TO_SOURCE, encoding: false })
-    .pipe(dest(PATH_TO_DIST));
+export function copyStatic() {
+  return src(PATHS_TO_STATIC, { base: PATH_TO_SOURCE, encoding: false }).pipe(
+    dest(PATH_TO_DIST),
+  );
 }
 
-export function startServer () {
-  const serveStatic = PATHS_TO_STATIC
-    .filter((path) => path.startsWith('!') === false)
-    .map((path) => {
-      const dir = path.replace(/(\/\*\*\/.*$)|\/$/, '');
-      const route = dir.replace(PATH_TO_SOURCE, '/');
+export function startServer() {
+  const serveStatic = PATHS_TO_STATIC.filter(
+    (path) => path.startsWith('!') === false,
+  ).map((path) => {
+    const dir = path.replace(/(\/\*\*\/.*$)|\/$/, '');
+    const route = dir.replace(PATH_TO_SOURCE, '/');
 
-      return { route, dir };
-    });
-
-  server.init({
-    server: {
-      baseDir: PATH_TO_DIST
-    },
-    serveStatic,
-    cors: true,
-    notify: false,
-    ui: false,
-  }, (err, bs) => {
-    bs.addMiddleware('*', (req, res) => {
-      res.write(readFileSync(`${PATH_TO_DIST}404.html`));
-      res.end();
-    });
+    return { route, dir };
   });
+
+  server.init(
+    {
+      server: {
+        baseDir: PATH_TO_DIST,
+      },
+      serveStatic,
+      cors: true,
+      notify: false,
+      ui: false,
+    },
+    (err, bs) => {
+      bs.addMiddleware('*', (req, res) => {
+        res.write(readFileSync(`${PATH_TO_DIST}404.html`));
+        res.end();
+      });
+    },
+  );
 
   watch(`${PATH_TO_SOURCE}**/*.{html,njk}`, series(processMarkup));
   watch(`${PATH_TO_SOURCE}styles/**/*.scss`, series(processStyles));
@@ -161,12 +167,12 @@ export function startServer () {
   watch(PATHS_TO_STATIC, series(reloadServer));
 }
 
-function reloadServer (done) {
+function reloadServer(done) {
   server.reload();
   done();
 }
 
-export function removeBuild (done) {
+export function removeBuild(done) {
   rmSync(PATH_TO_DIST, {
     force: true,
     recursive: true,
@@ -174,7 +180,7 @@ export function removeBuild (done) {
   done();
 }
 
-export function buildProd (done) {
+export function buildProd(done) {
   isDevelopment = false;
   series(
     removeBuild,
@@ -188,15 +194,10 @@ export function buildProd (done) {
   )(done);
 }
 
-export function runDev (done) {
+export function runDev(done) {
   series(
     removeBuild,
-    parallel(
-      processMarkup,
-      processStyles,
-      processScripts,
-      createStack,
-    ),
+    parallel(processMarkup, processStyles, processScripts, createStack),
     startServer,
   )(done);
 }
